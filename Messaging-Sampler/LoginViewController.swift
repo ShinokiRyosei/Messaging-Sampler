15aad05cf254dbbe8be44f2004be145145ef0b43
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-09-01 06:21:31 +0900

    [Add] textfield and ibaction

--- Messaging-Sampler/LoginViewController.swift
+++ Messaging-Sampler/LoginViewController.swift
@@ -9,15 +9,35 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        passwordTextField.secureTextEntry = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didSelectLogin() {
+        
+    }
+}
+
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
--- Messaging-Sampler/SignupViewController.swift
+++ Messaging-Sampler/SignupViewController.swift
@@ -9,15 +9,42 @@
 import UIKit
 
 class SignupViewController: UIViewController {
+    
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    @IBOutlet var passwordAgainTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        passwordAgainTextField.delegate = self
+        
+        passwordTextField.secureTextEntry = true
+        passwordAgainTextField.secureTextEntry = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didSelectSignup() {
+        
+    }
+    
+    @IBAction func didSelectToLogin() {
+        
+    }
+}
+
+extension SignupViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
