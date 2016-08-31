4a90ffcb28e81ecb7ded3aa0500ea3c4018596e6
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-09-01 05:51:36 +0900

    [Create] Auth View

---
+++ Messaging-Sampler/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  Messaging-Sampler
+//
+//  Created by ShinokiRyosei on 2016/09/01.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
