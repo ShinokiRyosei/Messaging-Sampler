310b6a08c5973aa5a5075275273ce0472904e5dd
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-09-03 19:27:58 +0900

    [Fix] JSQMessagesViewController

--- Messaging-Sampler/MessageViewController.swift
+++ Messaging-Sampler/MessageViewController.swift
@@ -11,9 +11,27 @@ import Firebase
 import JSQMessagesViewController
 
 class MessageViewController: JSQMessagesViewController {
+    
+    var messages: [JSQMessage]?
+    var incomingBubble: JSQMessagesBubbleImage!
+    var outgoingBubble: JSQMessagesBubbleImage!
+    var incomingAvatar: JSQMessagesAvatarImage!
+    var outgoingAvatar: JSQMessagesAvatarImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.senderId  = "user1"
+        self.senderDisplayName = "hoge"
+        
+        let bubbleFactory: JSQMessagesBubbleImageFactory = JSQMessagesBubbleImageFactory()
+        self.incomingBubble = bubbleFactory.incomingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleLightGrayColor())
+        self.outgoingBubble = bubbleFactory.outgoingMessagesBubbleImageWithColor(UIColor.jsq_messageBubbleGreenColor())
+        
+        self.incomingAvatar = JSQMessagesAvatarImageFactory.avatarImageWithImage(UIImage(named: "avatar"), diameter: 64)
+        self.outgoingAvatar = JSQMessagesAvatarImageFactory.avatarImageWithImage(UIImage(named: "avatar-1"), diameter: 64)
+        
+        self.messages = []
 
         // Do any additional setup after loading the view.
     }
@@ -21,5 +39,54 @@ class MessageViewController: JSQMessagesViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
+        let message = JSQMessage(senderId: senderId, displayName: senderDisplayName, text: text)
+        self.messages?.append(message)
+        
+        self.finishReceivingMessageAnimated(true)
+        
+        self.receiveAutoMessage()
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        return self.messages?[indexPath.item]
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (self.messages?.count)!
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageAvatarImageDataSource! {
+        let message = self.messages?[indexPath.item]
+        if message?.senderId == self.senderId {
+            return self.outgoingAvatar
+        }
+        return self.incomingAvatar
+    }
+//    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+//        let message = self.messages?[indexPath.item]
+//        if message?.senderId == self.senderId {
+//            return self.outgoingBubble
+//        }
+//        return self.incomingBubble
+//    }
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageBubbleImageDataSource! {
+        let message = self.messages?[indexPath.item]
+        if message?.senderId == self.senderId {
+            return self.outgoingBubble
+        }
+        return self.incomingBubble
+    }
+    
+    func receiveAutoMessage() {
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(self.didFinishMessageTimer(_:)), userInfo: nil, repeats: true)
+    }
+    
+    func didFinishMessageTimer(sender: NSTimer) {
+        let message: JSQMessage = JSQMessage(senderId: "user2", displayName: "underscore", text: "Hello!")
+        self.messages?.append(message)
+        self.finishReceivingMessageAnimated(true)
     }
 }
