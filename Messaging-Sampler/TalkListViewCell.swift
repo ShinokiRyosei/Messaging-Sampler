466120e13a62dfccf3fc6bbeaf554c6475acc492
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-09-03 19:39:58 +0900

    [Create] TalkListViewCell

---
+++ Messaging-Sampler/TalkListViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TalkListViewCell.swift
+//  Messaging-Sampler
+//
+//  Created by ShinokiRyosei on 2016/09/03.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class TalkListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Messaging-Sampler/TalkListViewExtension.swift
+++ Messaging-Sampler/TalkListViewExtension.swift
@@ -8,4 +8,14 @@
 
 import UIKit
 
-extension TalkListViewController
+extension TalkListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: TalkListViewCell = tableView.dequeueReusableCellWithIdentifier("") as! TalkListViewCell
+        
+        return cell
+    }
+}
