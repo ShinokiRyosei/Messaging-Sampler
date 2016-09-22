75a47280febf8e605cd2356095403d4ef4ac05a2
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-09-23 00:09:55 +0900

    [Create] Message.swift in Model

---
+++ Messaging-Sampler/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Messaging-Sampler
+//
+//  Created by ShinokiRyosei on 2016/09/23.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+struct Message {
+    <#fields#>
+}
