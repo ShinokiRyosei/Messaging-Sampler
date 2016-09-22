cdb3c6a1e6a9460915ddc01da1225692f7d61986
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-09-22 23:08:01 +0900

    [Create] SearchViewController

---
+++ Messaging-Sampler/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Messaging-Sampler
+//
+//  Created by ShinokiRyosei on 2016/09/22.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
