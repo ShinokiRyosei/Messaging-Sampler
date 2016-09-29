4d1cc902450b3d18ec485acae1aa8a09c616871f
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-09-29 19:07:22 +0900

    [Create] Username TextField

---
+++ Messaging-Sampler/UsernameTextFieldDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  UsernameTextFieldDelegate.swift
+//  Messaging-Sampler
+//
+//  Created by ShinokiRyosei on 2016/09/29.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class UsernameTextFieldDelegate: NSObject {
+
+}
