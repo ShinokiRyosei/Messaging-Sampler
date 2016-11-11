66ad52b936a889b61d29020f6dd9cf68f6cf2779
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-11-12 08:44:38 +0900

    [Create] Room model

---
+++ Messaging-Sampler/Room.swift
@@ -0,0 +1,13 @@
+//
+//  Room.swift
+//  Messaging-Sampler
+//
+//  Created by RS on 2016/11/12.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+struct Room {
+    
+}
